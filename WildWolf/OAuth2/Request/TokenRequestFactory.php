3427f689f3f9b5128dbf29c91602a1931ec2aeb1
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2017-12-07 23:52:31 +0200

    Refactoring

--- WildWolf/OAuth2/Request/TokenRequestFactory.php
+++ WildWolf/OAuth2/Request/TokenRequestFactory.php
@@ -6,17 +6,19 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class TokenRequestFactory
 {
+    public static $class_map = [
+        'authorization_code' => AuthorizationCodeRequest::class,
+        'password'           => PasswordRequest::class,
+        'client_credentials' => ClientCredentialsRequest::class,
+        'refresh_token'      => RefreshTokenRequest::class,
+    ];
+
     public static function create(ServerRequestInterface $req)
     {
         $params     = $req->getParsedBody();
         $grant_type = $params['grant_type'] ?? null;
+        $class      = self::$class_map[$grant_type] ?? BaseTokenRequest::class;
 
-        switch ($grant_type) {
-            case 'authorization_code': return AuthorizationCodeRequest::fromRequest($req);
-            case 'password':           return PasswordRequest::fromRequest($req);
-            case 'client_credentials': return ClientCredentialsRequest::fromRequest($req);
-            case 'refresh_token':      return RefreshTokenRequest::fromRequest($req);
-            default:                   return BaseTokenRequest::fromRequest($req);
-        }
+        return $class::fromRequest($req);
     }
 }
